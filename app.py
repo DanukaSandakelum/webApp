c7218d6afd98c25ee0f09d430cbcef5aa8a70604
DanukaSandakelum <sandakelumdanuka384@gmail.com>
2025-05-01 18:38:51 +0000

    Flask project

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Wo!</p>"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4370, debug=True)
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0',debug=True)
